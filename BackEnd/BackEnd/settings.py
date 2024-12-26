b10197d72a93b3c386bc4b7f3ed7f4de2fb1150d
zahra alizadeh <zahra121281alizadeh@gmail.com>
2024-12-26 15:56:25 +0330

    add-test-db

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -219,7 +219,7 @@ DATABASES = {
         'NAME': 'TherapyDB',
         'USER': 'postgres',
         'PASSWORD': 'Hgbr@@5391',
-        'HOST': 'postgres',
+        'HOST': 'postgreSQL',
         # 'HOST': 'localhost',
         'PORT': '5432',
         'DISABLE_SERVER_SIDE_CURSORS': True,
